c84724a36a79593636c78c7ee60e235651169f40
Christian Gabriana <142938707+christiangabr@users.noreply.github.com>
2023-10-03 15:43:06 -0500

    volume menu added

--- pause_menu.gd
+++ pause_menu.gd
@@ -7,3 +7,6 @@ func _on_resume_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_volume_pressed():
+	main.get_volume_menu()
---
+++ volume_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var main = $"../../"
+
+
+
+
+func _on_back_pressed():
+	main.pause()
