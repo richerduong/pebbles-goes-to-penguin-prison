16f82b9a1168cc80ef8d37ff0e32d9ee3c8e623e
Christian Gabriana <142938707+christiangabr@users.noreply.github.com>
2023-10-05 13:22:42 -0500

    volume-menu-added

--- volume_menu.gd
+++ volume_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+<<<<<<< Updated upstream
 @onready var main = $"../../"
 
 
@@ -7,3 +8,18 @@ extends Control
 
 func _on_back_pressed():
 	main.pause()
+=======
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://options_menu.tscn")
+>>>>>>> Stashed changes
