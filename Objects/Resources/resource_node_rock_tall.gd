f59f23dd74423e79a2ce277d10f16f46656d9db9
Christine Do <thechristinedo@gmail.com>
2023-11-02 19:14:39 -0500

    added fish barrel interaction

--- Objects/Resources/resource_node_rock_tall.gd
+++ Objects/Resources/resource_node_rock_tall.gd
@@ -4,6 +4,8 @@ extends StaticBody2D
 @export var pickup_type : PackedScene
 
 @onready var level_parent = get_parent()
+@export var launch_speed : float = 100
+@export var launch_direction : float = 0.25
 
 # Hit Flash Shader
 @onready var sprite = $Sprite2D
@@ -29,3 +31,10 @@ func spawn_resource():
 	var pickup_instance : Pickup = pickup_type.instantiate() as Pickup
 	level_parent.add_child(pickup_instance)
 	pickup_instance.position = position
+	
+	var direction : Vector2 = Vector2 (
+		randf_range(-1.0, 1.0),
+		randf_range(-1.0, 1.0)
+	).normalized()
+	
+	pickup_instance.launch(direction * launch_speed, launch_direction)
